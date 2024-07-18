dac2bd61370c843a9b8a9d87f2d530f3a97ed41a
Andrés López <lopezherasandres>
2024-07-18 18:24:38 +0200

    ejercicio completo

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,36 @@
-function initContador() {
-  let contador = 0;
+let contador = 0;
 
-  // Defino las constantes del contador enlazando con los elementos del HTML
-  const contadorElement = document.getElementById("contador");
-  const btnAnterior = document.getElementById("btn-anterior");
-  const btnSiguiente = document.getElementById("btn-siguiente");
-  const btnReset = document.getElementById("btn-reset");
+// Defino las constantes del contador enlazando con los elementos del HTML
+const contadorElement = document.getElementById("contador");
+const btnAnterior = document.getElementById("btn-anterior");
+const btnSiguiente = document.getElementById("btn-siguiente");
+const btnReset = document.getElementById("btn-reset");
 
-  function actualizarContador() {
-    // Añado a la función el padStart para que el contador tenga X longitud
-    contadorElement.textContent = String(contador).padStart(2, "0");
-  }
+// Actualizar contador
+function actualizarContador() {
+  // Añado a la función el padStart para que el contador tenga X longitud
+  contadorElement.textContent = String(contador).padStart(2, "0");
+}
 
-  function cambiarContador(valor) {
-    contador += valor;
-    // Condicional para que no cuente en números negativos
-    if (contador < 0) {
-      contador = 0;
-    }
-    actualizarContador();
-  }
-  // Reinicio de contador
-  function resetContador() {
+// Cambio de contador
+function cambiarContador(valor) {
+  contador += valor;
+  // Condicional para que no cuente en números negativos
+  if (contador < 0) {
     contador = 0;
-    actualizarContador();
   }
-
-  btnAnterior.addEventListener("click", () => cambiarContador(-1));
-  btnSiguiente.addEventListener("click", () => cambiarContador(1));
-  btnReset.addEventListener("click", resetContador);
-
   actualizarContador();
 }
+
+// Reinicio de contador
+function resetContador() {
+  contador = 0;
+  actualizarContador();
+}
+
+// Eventos al hacer click
+btnAnterior.addEventListener("click", () => cambiarContador(-1));
+btnSiguiente.addEventListener("click", () => cambiarContador(1));
+btnReset.addEventListener("click", resetContador);
+
+actualizarContador();
