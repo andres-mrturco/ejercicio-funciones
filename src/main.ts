7e40346c4091dd0ffa81dfce3c3c31a2ae41680b
Andrés López <lopezherasandres>
2024-07-18 18:00:48 +0200

    actualizacion de funciones

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,34 @@
-import "./style.css";
+function initContador() {
+  let contador = 0;
 
-console.log("Hello Andres");
+  // Defino las constantes del contador enlazando con los elementos del HTML
+  const contadorElement = document.getElementById("contador");
+  const btnAnterior = document.getElementById("btn-anterior");
+  const btnSiguiente = document.getElementById("btn-siguiente");
+  const btnReset = document.getElementById("btn-reset");
+
+  function actualizarContador() {
+    // Añado a la función el padStart para que el contador tenga X longitud
+    contadorElement.textContent = String(contador).padStart(2, "0");
+  }
+
+  function cambiarContador(valor) {
+    contador += valor;
+    // Condicional para que no cuente en números negativos
+    if (contador < 0) {
+      contador = 0;
+    }
+    actualizarContador();
+  }
+  // Reinicio de contador
+  function resetContador() {
+    contador = 0;
+    actualizarContador();
+  }
+
+  btnAnterior.addEventListener("click", () => cambiarContador(-1));
+  btnSiguiente.addEventListener("click", () => cambiarContador(1));
+  btnReset.addEventListener("click", resetContador);
+
+  actualizarContador();
+}
