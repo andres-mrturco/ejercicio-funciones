8e3a4a8e8c2549fbae38beb1cc02b1bb1a1ee7fd
Andrés López <lopezherasandres>
2024-07-19 12:13:00 +0200

    corregidos errores null

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,65 @@ let contador = 0;
 
 // Defino las constantes del contador enlazando con los elementos del HTML
 const contadorElement = document.getElementById("contador");
-const btnAnterior = document.getElementById("btn-anterior");
-const btnSiguiente = document.getElementById("btn-siguiente");
-const btnReset = document.getElementById("btn-reset");
+if (contadorElement !== null && contadorElement !== undefined) {
+  actualizarContador();
+}
 
-//const inputTurno = document.getElementById("input-turno");
-//const btnCambiar = document.getElementById("btn-cambiar");
+const btnAnterior = document.getElementById("btn-anterior");
+if (
+  btnAnterior !== null &&
+  btnAnterior !== undefined &&
+  btnAnterior instanceof HTMLButtonElement
+) {
+  btnAnterior.addEventListener("click", () => cambiarContador(-1));
+}
+
+const btnSiguiente = document.getElementById("btn-siguiente");
+if (
+  btnSiguiente !== null &&
+  btnSiguiente !== undefined &&
+  btnSiguiente instanceof HTMLButtonElement
+) {
+  btnSiguiente.addEventListener("click", () => cambiarContador(1));
+}
+
+const btnReset = document.getElementById("btn-reset");
+if (
+  btnReset !== null &&
+  btnReset !== undefined &&
+  btnReset instanceof HTMLButtonElement
+) {
+  btnReset.addEventListener("click", resetContador);
+}
+
+/*const inputTurno = document.getElementById("input-turno");
+if (
+  inputTurno !== null &&
+  inputTurno !== undefined &&
+  inputTurno instanceof HTMLButtonElement
+) {
+  inputTurno.addEventListener("click", inputTurno);
+}
+const btnCambiar = document.getElementById("btn-cambiar");
+if (
+  btnCambiar !== null &&
+  btnCambiar !== undefined &&
+  btnCambiar instanceof HTMLButtonElement
+) {
+  btnCambiar.addEventListener("click", btnCambiar);
+}
+*/
 
 // Actualizar contador
 function actualizarContador() {
-  // Añado a la función el padStart para que el contador tenga X longitud (Extra)
-  contadorElement.textContent = String(contador).padStart(2, "0");
+  if (contadorElement !== null && contadorElement !== undefined) {
+    // Añado a la función el padStart para que el contador tenga X longitud (Extra)
+    contadorElement.textContent = `${contador}`.padStart(2, "0");
+  }
 }
 
 // Cambio de contador
-function cambiarContador(valor) {
+function cambiarContador(valor: number) {
   contador += valor;
   // Condicional para que no cuente en números negativos
   if (contador < 0) {
@@ -32,12 +76,5 @@ function resetContador() {
 }
 
 // function cambiarTurno() {}
-
-// Eventos al hacer click
-btnAnterior.addEventListener("click", () => cambiarContador(-1));
-btnSiguiente.addEventListener("click", () => cambiarContador(1));
-btnReset.addEventListener("click", resetContador);
-
-//btnCambiar.addEventListener("click", cambiarTurno);
 
 actualizarContador();
